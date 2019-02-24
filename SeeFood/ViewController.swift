b0e9f83bc018f1c401234839fde6a5b8ea5a1bb4
giuliaboscaro <giulia.boscaro@hotmail.com>
2019-02-23 22:34:04 -0300

    Update UI depending on the recognition result

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -28,19 +28,57 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    let hotDogView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage.init(named: "hotdogBackground")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let resultView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     func setupScreen() {
-        view.backgroundColor = UIColor.white
-        title = "Hotdog or not Hotdog?"
+        view.backgroundColor = UIColor.init(red: 38/255, green: 160/255, blue: 196/255, alpha: 1)
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 34/255, green: 127/255, blue: 155/255, alpha: 1)
+        self.navigationController?.navigationBar.tintColor = UIColor.black
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 30, weight: .bold), NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.strokeColor: UIColor.black, NSAttributedString.Key.strokeWidth: -3.0]
+        title = "Is this a Hotdog?"
         view.addSubview(imageView)
+        view.addSubview(hotDogView)
+        view.addSubview(resultView)
         
         imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
+        hotDogView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        hotDogView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        hotDogView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        hotDogView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        resultView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        resultView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        resultView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        resultView.heightAnchor.constraint(equalToConstant: 85).isActive = true
     }
     
+    func isHotDog() {
+        resultView.image = UIImage(named: "hotdog")
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 42/255, green: 253/255, blue: 0/255, alpha: 1)
+    }
     
-
+    func notHotDog() {
+        resultView.image = UIImage(named: "not-hotdog")
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 231/255, green: 62/255, blue: 50/255, alpha: 1)
+    }
 
 }
 
@@ -58,6 +96,7 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            hotDogView.removeFromSuperview()
             imageView.image = selectedImage
             
             guard let ciImage = CIImage(image: selectedImage) else {
@@ -83,8 +122,10 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
             if let firstResult = results.first {
                 if firstResult.identifier.contains("hotdog") {
                     self.navigationItem.title = "Hotdog"
+                    self.isHotDog()
                 } else {
                     self.navigationItem.title = "Not Hotdog"
+                    self.notHotDog()
                 }
             }
             
